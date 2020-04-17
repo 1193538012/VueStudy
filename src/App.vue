cf112ad8ad27a5b830b56778afc8776fa84aa5d3
jzy <1193538012@qq.com>
2020-04-17 08:53:45 +0800

    StudyVue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BasicGrammar msg="12321231212122"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import BasicGrammar from './components/01BasicGrammar.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,BasicGrammar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/01BasicGrammar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="01BasicGrammar">
+        <p>{{msg}}</p>
+        <p>{{msg2}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "01BasicGrammar",
+        props: {
+            msg: String
+        },
+        data() {
+            return{
+                msg2:'asdasdasd'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
